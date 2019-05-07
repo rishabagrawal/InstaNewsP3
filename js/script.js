f837965fcc0aa0944dbf735b84ab7015555dfde4
GabPhay21 <gjpjr_1992@icloud.com>
2019-05-06 18:40:55 -0700

    recovered lost articles

--- js/script.js
+++ js/script.js
@@ -1,29 +1,33 @@
 $(function() {
 
+  //  loadingIcon.hide();
+
   const loadingIcon=$('.loading');
   const articles=$('.articles');
 
-  loadingIcon.hide();
+  loadingIcon.show();
   $('.selection').on('change','.selectors',function(){
     const selectors=$(this).val();
-    if(selectors==='')return;
+    if (selectors===''){return}
     $.ajax({
           method:'get',
           url:'https://api.nytimes.com/svc/topstories/v2/' +selectors+ '.json?api-key=D5H6I9oboArY2CG6wjPNy9BAvjnLZOey',
         }).done(function(data) {
           articles.empty();
-          loadingIcon.hide();
+          // loadingIcon.hide();
           //const results=data.results;
           const results=data.results.filter((element)=>{return element.multimedia;}).slice(0,12);
-      $.each(results, function(key, value) {
-        const title=value.title;
+      $.each(results, function(_key, value) {
         const link=value.multimedia[4].url;
         const text=value.abstract;
-        const html='<img src='+link+'><h1>'+title+'</h1><p>'+text+'</p>';
+        const html='<img src='+link+'><p>'+text+'</p>';
         articles.append(html);
+   
       });
+
+      // 
     }).always(function(){
-      loadingIcon.show();
+      loadingIcon.hide();
     }
     )
   });
