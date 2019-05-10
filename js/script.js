e22ff324bde5b9be5f1d78e48b95721e69240c37
GabPhay21 <gjpjr_1992@icloud.com>
2019-05-09 20:47:55 -0700

    format overhual

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@ $(function() {
 
   //  loadingIcon.hide();
 
-  const loadingIcon=$('.loading');
+  const loadingIcon=$('.loader');
   const articles=$('.articles');
 
-  loadingIcon.show(1500);
+  loadingIcon.hide();
   $('.selection').on('change','.selectors',function(){
+    loadingIcon.show()
     const selectors=$(this).val();
     if (selectors===''){return}
     $.ajax({
@@ -15,9 +16,10 @@ $(function() {
           url:'https://api.nytimes.com/svc/topstories/v2/' +selectors+ '.json?api-key=D5H6I9oboArY2CG6wjPNy9BAvjnLZOey',
         }).done(function(data) {
           articles.empty();
-          //  loadingIcon.hide();
+          loadingIcon.hide();
+          
          
-          const results=data.results.filter((element)=>{return element.multimedia;}).slice(0,12);
+          const results=data.results.filter((element)=>{return element.multimedia[4];}).slice(0,12);
           // console.log(results);
       $.each(results, function(_key, value) {
         const url=value.url;
@@ -29,7 +31,7 @@ $(function() {
 
       
     }).always(function(){
-      loadingIcon.hide(50);
+     
     }
     )
   });
